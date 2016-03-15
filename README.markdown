2658fa4ffb287903e12851636c223252ceaffa94
Kyle Kelley <rgbkrk@gmail.com>
2016-03-15 12:03:45 -0500

    Time for spectacle.

---
+++ README.markdown
@@ -0,0 +1,19 @@
+# JupyterHub Boston 2015
+
+## Reference
+
+The Spectacle core API is available at [https://github.com/FormidableLabs/spectacle/blob/master/README.markdown](https://github.com/FormidableLabs/spectacle/blob/master/README.markdown).
+
+## Development
+
+After cloning this repository, your first order of business is to open the terminal and run `npm install`
+
+Then, to start up the local server, run `npm start`
+
+Open a browser and hit [http://localhost:3000](http://localhost:3000), and we are ready to roll
+
+## Build & Deployment
+
+To build the deck for publishing, run `npm run build`.
+
+If you want to deploy the slideshow to surge, run `npm run deploy`
---
+++ presentation/index.js
@@ -0,0 +1,76 @@
+// Import React
+import React from 'react';
+
+// Import Spectacle Core tags
+import {
+  Appear,
+  BlockQuote,
+  Cite,
+  CodePane,
+  Deck,
+  Fill,
+  Heading,
+  Image,
+  Layout,
+  Link,
+  ListItem,
+  List,
+  Markdown,
+  Quote,
+  Slide,
+  Spectacle,
+  Text,
+} from 'spectacle';
+
+// Import image preloader util
+import preloader from 'spectacle/lib/utils/preloader';
+
+// Import theme
+import createTheme from 'spectacle/lib/themes/default';
+
+// Import custom component
+import Interactive from '../assets/interactive';
+
+// Require CSS
+require('normalize.css');
+require('spectacle/lib/themes/default/index.css');
+
+
+const images = {
+  city: require('../assets/city.jpg'),
+  kat: require('../assets/kat.png'),
+  logo: require('../assets/formidable-logo.svg'),
+  markdown: require('../assets/markdown.png'),
+};
+
+preloader(images);
+
+const theme = createTheme({
+  primary: '#ff8a40',
+});
+
+export default class Presentation extends React.Component {
+  render() {
+    return (
+      <Spectacle theme={theme}>
+        <Deck transition={['zoom', 'slide']} transitionDuration={500}>
+          <Slide transition={['zoom']} bgColor="primary">
+            <Heading size={1} fit caps lineHeight={1} textColor="black">
+              JupyterHub
+            </Heading>
+            <Heading size={1} fit caps>
+              On Carina
+            </Heading>
+            <Heading size={2} fit caps textColor="black">
+              at JupyterDays Boston 2015
+            </Heading>
+            <Link href="https://github.com/getcarina/jupyterhub-boston-2015">
+              <Text bold caps textColor="tertiary">View on Github</Text>
+            </Link>
+            <Text textSize="1.5em" margin="20px 0px 0px" bold>Hit Your Right Arrow To Begin!</Text>
+          </Slide>
+        </Deck>
+      </Spectacle>
+    );
+  }
+}
